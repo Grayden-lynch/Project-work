e5ea886a9f54fde743a966f45af3fbcf33964a39
Grayden-lynch <grayden.lynch@mnsu.edu>
2026-09-16 09:40:47 -0500

    Implement expense tracking in Project.py
    Added expense input and calculation loop to track balance.

--- Project.py
+++ Project.py
@@ -3,6 +3,12 @@ print(f"Hello {login} welcome to SpendSmart")
 
 paycheck = float(input("Enter paycheck amount:"))
 balance = paycheck
+expense = float(input("Enter expense(0 to stop):"))
+
+while expense <= 0:
+  balance = balance - expense
+  expense = float(input("Enter expense(0 to stop):"))
+print(f"Your final balance is {balance}.")
 
 
 
