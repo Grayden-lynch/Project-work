8a0ae89e1cad89d24d989cb779cd7c302a4776c4
Grayden-lynch <grayden.lynch@mnsu.edu>
2026-09-16 09:48:11 -0500

    Fix expense loop condition to allow zero expense

--- Project.py
+++ Project.py
@@ -5,7 +5,7 @@ paycheck = float(input("Enter paycheck amount:"))
 balance = paycheck
 expense = float(input("Enter expense(0 to stop):"))
 
-while expense <= 0:
+while expense != 0:
   balance = balance - expense
   expense = float(input("Enter expense(0 to stop):"))
 print(f"Your final balance is {balance}.")
